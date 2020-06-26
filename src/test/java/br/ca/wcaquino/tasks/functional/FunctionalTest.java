3f4b34df9acc4ca018075b0898abf71fa0d9eafe
eduardofreitascarneiro <eduardofreitascarneiro@gmail.com>
2020-06-26 15:00:56 -0300

    Alterando a classe de teste funcional, retirando o @Before

--- src/test/java/br/ca/wcaquino/tasks/functional/FunctionalTest.java
+++ src/test/java/br/ca/wcaquino/tasks/functional/FunctionalTest.java
@@ -2,9 +2,7 @@ package br.ca.wcaquino.tasks.functional;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,90 +10,104 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FunctionalTest {
 
-	WebDriver driver;
 	
-	@Before
-	public void before() {
-		driver = new ChromeDriver();
-	}
-	
+	public WebDriver acessarAplicacao() {
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://localhost:8001/tasks");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}	
 	
 	@Test
 	public void deveSalvarTarefaComSucesso() {
 		
-		driver.navigate().to("http://localhost:8001/tasks");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriver driver = acessarAplicacao();
 		
-		//Clicar no botão para adicionar nova tarefa
-		driver.findElement(By.id("addTodo")).click();
+		try {
+			
+			//Clicar no botão para adicionar nova tarefa
+			driver.findElement(By.id("addTodo")).click();
+			
+			//Preencher o campo descrição
+			driver.findElement(By.id("task")).sendKeys("Descricao automatizada");
+			
+			//Preencher o campo data
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2100");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//Verificar mensagem de sucesso.
+			String Mensagem = driver.findElement(By.id("message")).getText().toString();
+			
+			Assert.assertEquals("Success!", Mensagem);
+		} finally {
+			driver.quit();	
+		}
 		
-		//Preencher o campo descrição
-		driver.findElement(By.id("task")).sendKeys("Descricao automatizada");
-				
-		//Preencher o campo data
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/2100");
-		
-		//clicar em salvar
-		driver.findElement(By.id("saveButton")).click();
-		
-		//Verificar mensagem de sucesso.
-		String Mensagem = driver.findElement(By.id("message")).getText().toString();
-		
-		Assert.assertEquals("Success!", Mensagem);
 	}
 	
 	@Test
 	public void naoDeveSalvarTarefaSemNome() {
+		WebDriver driver = acessarAplicacao();
 		
-		driver.navigate().to("http://localhost:8001/tasks");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		try {
+			
+			driver.navigate().to("http://localhost:8001/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
+			//Clicar no botão para adicionar nova tarefa
+			driver.findElement(By.id("addTodo")).click();
+			
+			//Preencher o campo descrição
+			//driver.findElement(By.id("task")).sendKeys("Descricao automatizada");
+			
+			//Preencher o campo data
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2100");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//Verificar mensagem de sucesso.
+			String Mensagem = driver.findElement(By.id("message")).getText().toString();
+			
+			Assert.assertEquals("Fill the task description", Mensagem);
+		} finally {
+			driver.quit();	
+		}
 		
-		//Clicar no botão para adicionar nova tarefa
-		driver.findElement(By.id("addTodo")).click();
-		
-		//Preencher o campo descrição
-		//driver.findElement(By.id("task")).sendKeys("Descricao automatizada");
-				
-		//Preencher o campo data
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/2100");
-		
-		//clicar em salvar
-		driver.findElement(By.id("saveButton")).click();
-		
-		//Verificar mensagem de sucesso.
-		String Mensagem = driver.findElement(By.id("message")).getText().toString();
-		
-		Assert.assertEquals("Fill the task description", Mensagem);
 	}	
 	
 	
 	@Test
 	public void naoDeveSalvarTarefaComDataPassada() {
+		WebDriver driver = acessarAplicacao();
+		try {
+			driver.navigate().to("http://localhost:8001/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
+			//Clicar no botão para adicionar nova tarefa
+			driver.findElement(By.id("addTodo")).click();
+			
+			//Preencher o campo descrição
+			driver.findElement(By.id("task")).sendKeys("Descricao automatizada");
+			
+			//Preencher o campo data
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2000");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//Verificar mensagem de sucesso.
+			String Mensagem = driver.findElement(By.id("message")).getText().toString();
+			
+			Assert.assertEquals("Due date must not be in past", Mensagem);
+			
+		} finally {
+			driver.quit();
+		}
 		
-		driver.navigate().to("http://localhost:8001/tasks");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		//Clicar no botão para adicionar nova tarefa
-		driver.findElement(By.id("addTodo")).click();
-		
-		//Preencher o campo descrição
-		driver.findElement(By.id("task")).sendKeys("Descricao automatizada");
-				
-		//Preencher o campo data
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/2000");
-		
-		//clicar em salvar
-		driver.findElement(By.id("saveButton")).click();
-		
-		//Verificar mensagem de sucesso.
-		String Mensagem = driver.findElement(By.id("message")).getText().toString();
-		
-		Assert.assertEquals("Due date must not be in past", Mensagem);
 	}	
 	
-	@After
-	public void after() {
-		driver.quit();
-	}
 	
 }
